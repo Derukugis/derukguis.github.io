327b486333ccc8a8dcf935bc2507b4f3ccda7beb
Derukugi <159861671+Derukugis@users.noreply.github.com>
2024-02-20 09:17:34 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+    function coordinate(event) {
+        let x = event.clientX;
+        let y = event.clientY;
+        document.getElementById("X").value = x;
+        document.getElementById("Y").value = y;
+    }
