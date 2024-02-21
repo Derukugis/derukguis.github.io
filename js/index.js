dfd21d51f78a11dbd7c62841ae415e35ac8d48d3
Derukugi <159861671+Derukugis@users.noreply.github.com>
2024-02-21 14:01:36 +1100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}
 
 var bubble = document.getElementBy("gravity")
 bubble.style.position = "absolute";
-bubble.style.top = y_pos+'20';
+bubble.style.top = y_pos+'500';
